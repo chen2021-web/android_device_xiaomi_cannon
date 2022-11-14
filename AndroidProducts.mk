05e9cc8de79e648f2dd5cecfd337ea86ab260fe1
chen2021-web <83762621+chen2021-web@users.noreply.github.com>
2022-11-15 07:43:58 +0800

    进行ify操作

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The aospOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cannon.mk
+    $(LOCAL_DIR)/aosp_cannon.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_cannon-user \
-    lineage_cannon-userdebug \
-    lineage_cannon-eng
+    aosp_cannon-user \
+    aosp_cannon-userdebug \
+    aosp_cannon-eng
